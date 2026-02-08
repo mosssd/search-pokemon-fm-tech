230696c13e05a999ed9e3420809dd75097cd4b39
saranrak <saranrak.d@ku.th>
2026-02-08 18:51:49 +0700

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import PokemonSearch from "@/components/PokemonSearch";
 
 export default function Page() {
-  return <PokemonSearch />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PokemonSearch />
+    </Suspense>
+  );
 }
